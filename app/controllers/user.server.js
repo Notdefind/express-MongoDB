f7003caebc9af77a07c5e0772db5a82e4c9799aa
王康 <fier666@163.com>
2017-12-05 15:37:13 +0800

    增删改查

--- app/controllers/user.server.js
+++ app/controllers/user.server.js
@@ -1,31 +1,71 @@
 import url from 'url';
 
-function login (db) { 
+function userServer (db) { 
   const user = db.collection('user');
-  
-  this.checkedUser = (req, res) => {
-    const params = req.body
-    const { userNanme, password } = params
 
-    user.insert({userNanme, password }, function (err) {
-        if (err) {
-          throw err;
-        } else {
-          res.end(`success`)
-        }
+  this.create = async (req, res) => {
+    const params = req.query;
+    const { userName, password } = params;
+    
+    if (!userName || !password) {
+      res.send("缺少参数");
+      return false;
+    }
+
+    const hasUserName = await user.findOne({userName})
+
+    if (hasUserName) {
+      res.send("用户名存在");
+    } else {
+      const data = await user.insert({ userName, password })
+      res.send("创建成功");
+    }
+
+  };
+
+  this.update = async (req, res) => {
+    const params = req.query;
+    const { userName, password } = params;
+    const hasUserName = await user.findOne({userName})
+
+    if(hasUserName) {
+      const updateUser = await user.update({userName},{$set: {password}})
+      res.send("更新成功");
+    } else {
+      res.send("用户名不存在");
+    } 
+  }
+
+  this.userList = async (req, res) => {
+    const userList = await user.find();
+
+    userList.toArray((err, result) => {
+      res.send(result);
     });
-  }
+  };
 
-  this.userList = (req, res) => {
-    user.find({userName: 'wangkang'}).toArray((err, result) => {
-      if (err) {
-        throw err;
-      } else {
-        res.send(result)
-      }
-    })
-  }
+  this.deleteAll = async(req, res) => {
+    const userList = await user.remove();
+
+    res.send("全部删除成功");
+  };
+
+  this.deleteOne = async (req, res) => {
+    const params = req.query;
+    const { userName } = params;
+    const hasUserName = await user.findOne({userName})
+
+    if (hasUserName) {
+      const userList = await user.remove({userName});
+
+      res.send("单个删除成功");
+    } else {
+      res.send("用户名不存在");
+    }
+    
+  };
+
 }
   
-  export default login;
+export default userServer;
   
--- app/routes/server/user.js
+++ app/routes/server/user.js
@@ -1,11 +1,18 @@
 import userServer from '../../controllers/user.server';
 
 function user (app, db) {
-  const login = new userServer(db);
+  const user = new userServer(db);
+  const { create, userList, update, deleteAll, deleteOne } = user;
 
-  app.route('/api/createUser').get(login.checkedUser)
+  app.route("/api/user/create").get(create);
   
-  app.route('/api/userList').get(login.userList)
+  app.route("/api/user/getList").get(userList);
+
+  app.route("/api/user/update").get(update);
+
+  app.route("/api/user/deleteAll").get(deleteAll);
+
+  app.route("/api/user/deleteOne").get(deleteOne);
 }
 
 export default user;
